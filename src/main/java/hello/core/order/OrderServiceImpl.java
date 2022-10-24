bf900dc75acd4e4fc9b97b413f9002db6b95f24e
박은빈 <joypeb@naver.com>
2022-10-24 22:31:01 +0900

    할인 추가, 관심사 분리 DI

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,26 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    /*private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+      private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+      구현 클래스에 의존을 하고있다
+
+      배우는 본인의 역할인 배역에만 집중해야된다
+      담당 배우를 섭외하는 공연기획자를 따로 만들자*/
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
