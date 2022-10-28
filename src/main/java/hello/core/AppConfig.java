eed1ae5d85a7ec129683cc1b60dac912cd0f19c3
박은빈 <joypeb@naver.com>
2022-10-28 14:45:36 +0900

    스프링 빈 테스트

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -17,16 +17,19 @@ public class AppConfig {
 
     @Bean
     public MemberService memberService() {
+        System.out.println("memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
